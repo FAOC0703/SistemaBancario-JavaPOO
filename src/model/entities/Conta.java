07bd84f47c35624c725fa8c549cdb3b58ba3bc11
FAOC0703 <fo7499490@gmail.com>
2026-09-14 15:44:01 -0300

    feat: adicionado as funcionalidades da classe Conta

--- src/model/entities/Conta.java
+++ src/model/entities/Conta.java
@@ -2,8 +2,41 @@ package model.entities;
 
 public abstract class Conta {
 	
+	private Integer numero;
+	private Double saldo;
+	private Cliente titular;
+	
+	public Conta() {}
 
+	public Conta(Integer numero, Cliente titular) {
+		this.numero = numero;
+		this.titular = titular;
+	}
 	
+	public Conta(Integer numero, Double saldo, Cliente titular) {
+		this.numero = numero;
+		this.saldo = saldo;
+		this.titular = titular;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
 	
+	public void depositar(double quantia) {
+		saldo += quantia;
+	}
+	
+	public void sacar(double quantia) {
+		saldo -= quantia;
+	}
 
 }
